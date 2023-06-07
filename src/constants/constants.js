1abdbc5003d9f07a137044a3f237a0ed853e8963
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-06-07 23:32:38 +0700

    add constant file

---
+++ src/constants/constants.js
@@ -0,0 +1,14 @@
+export const FEATURES_DATA = [
+    {
+        headline: "Watch everywhere",
+        description: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+    },
+    {
+        headline: "Watch on your TV",
+        description: "Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+    },
+    {
+        headline: "Explore more movies and TV shows",
+        description: "Login with your account to explore more fantastic movies.",
+    },
+]
