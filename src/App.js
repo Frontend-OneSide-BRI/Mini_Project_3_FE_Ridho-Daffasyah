20ec408e3be235740c856fcbb8dce77c6d1d339c
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-06-07 23:44:40 +0700

    add router to app for navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
+//Import react router dom
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './App.css';
 
 import Homepage from './pages/Homepage';
+import MoviesPages from './pages/Movies';
 
 const App = () =>  {
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/movies" element={<MoviesPages/>} />
+        {/* {
+          data.map((item, index) => {
+            return (
+              <Route key={index} path={`/detail/${item.name}`} element={<DetailPage data={data} item={item} />} />
+            )
+          }
+        )} */}
+      </Routes>
+    </BrowserRouter>  
   );
 }
 
