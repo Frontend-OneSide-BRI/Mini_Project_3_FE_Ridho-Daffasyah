9dfb0c86e42adb3671f15623f7f42fd4a310e990
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-06-07 23:44:17 +0700

    add dummy movies page

---
+++ src/pages/Movies/index.jsx
@@ -0,0 +1,12 @@
+//Movies pages
+import React from 'react';
+
+const MoviesPages = () => {
+    return (
+        <div className='h-screen w-full bg-black'>
+            <h1 className="text-white text-4xl">Movies Pages</h1>
+        </div>
+    );
+};
+
+export default MoviesPages;
