bfd5cc5b5639b92f7eb18faec4d4e8cb1ca35d65
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-06-07 23:34:07 +0700

    add homepage page

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -2,25 +2,29 @@
 import React from 'react';
 
 //Import components
-import Navbar from 'src/components/molecules/Navbar';
+import { Navbar, Banner, Footer } from 'src/components/molecules';
+import Line from 'src/components/atom/Line';
+import { Features } from 'src/components/molecules';
+
+//import data
+import { FEATURES_DATA } from 'src/constants/constants';
 
 const Homepage = () => {
     return (
         <div className='h-screen w-full'>
             <Navbar />
-            <div className='h-full w-full bg-red-50'>
-                <div className='h-full w-full flex flex-row items-center justify-center'>
-                    <div className='h-full w-full flex flex-col items-center justify-center'>
-                        {/* <video className='h-full w-full object-cover opacity-[0.4]' src='/Haikyuu-Final.mp4' autoPlay loop muted /> */}
-                        <div className='h-full w-full absolute z-10 flex flex-col items-center justify-center'>
-                            <h1 className='font-extrabold text-blue-950 text-4xl drop-shadow-xl'>Unlimited movies, TV shows, and more</h1>
-                            <span className='font-bold text-blue-950 text-2xl mt-[10px]'>Watch anywhere. Cancel anytime.</span>
-                            <span className='font-bold text-blue-950 text-2xl mt-[10px]'>Ready to watch? Let's explore the movies.</span>
-                            <button className='h-[50px] w-[180px] rounded-md bg-blue-950 mt-[50px] text-white font-bold'>Explore</button>
-                        </div>
-                    </div>
-                </div>
+            <Banner />
+            <Line />
+            <div className='w-full flex flex-row items-center sm:px-36 px-0 py-10 bg-black'>
+                <h1 className='font-extrabold text-white text-xl sm:text-4xl m-auto'>Features</h1>
             </div>
+            <Line />
+            {
+                FEATURES_DATA.map((feature, index) => {
+                    return <Features feature={feature} index={index} />
+                })
+            }
+            <Footer />
         </div>
     );
 };
