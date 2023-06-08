5ed0e83ac77d74c8cdb2079e428a7e8fc621a198
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-06-08 23:15:09 +0700

    add molecules components

---
+++ src/components/molecules/FormLogin/index.jsx
@@ -0,0 +1,44 @@
+//FormLogin Components
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+//Import Components
+import { Button } from 'src/components/atom';
+
+const FormLogin = ({ reqToken }) => {
+    const dispatch = useDispatch();
+
+    const [userData, setUserData] = useState({
+        username: '',
+        password: '',
+        request_token: ''
+    });
+
+    const handleChange = (e) => {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    return (
+        <form action="post" className='w-[30%] bg-black rounded-xl opacity-[0.8] p-10 absolute z-[1] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+            <h1 className='text-4xl font-bold text-white !text-left'>Login</h1>
+            <div className='w-full flex flex-col items-center'>
+                <div className='w-full flex flex-col items-start mt-10'>
+                    <label className='text-white font-bold text-xl' htmlFor="username">Username</label>
+                    <input className='w-full border-2 border-black rounded-md px-2 py-1 mt-2' type="text" name="username" id="username" onChange={handleChange} />
+                </div>
+                <div className='w-full flex flex-col items-start mt-5'>
+                    <label className='text-white font-bold text-xl' htmlFor="password">Password</label>
+                    <input className='w-full border-2 border-black rounded-md px-2 py-1 mt-2' type="password" name="password" id="password" onChange={handleChange}/>
+                </div>
+                <div className='w-full flex flex-col items-center mt-5'>
+                    <Button text="Sign In"/>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default FormLogin;
--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -1,17 +1,43 @@
 //Navbar components
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 //Import components
 import Button from 'src/components/atom/Button';
 
-const Navbar = () => {
+const Navbar = ({isLogin}) => {
     return (
         <nav
             className="h-24 w-full flex flex-row items-center justify-between py-8 sm:px-36 absolute z-[1]"
         >
             <div className='h-full w-full flex flex-col sm:flex-row items-center justify-between gap-6 sm:gap-0'>
-                <img src="/logo/logo-aniflix.png" alt="Logo Aniflix" className='w-[100px] sm:w-[150px]' />
-                <Button text="Login"/>
+                <Link to="/">
+                    <img src="/logo/logo-aniflix.png" alt="Logo Aniflix" className='w-[100px] sm:w-[150px]' />
+                </Link>
+                {(!isLogin && isLogin !== null) && <Button text="Login" />}
+                {
+                    isLogin &&
+                    <div className='flex flex-row gap-6'>
+                            <Link className="text-white font-bold" to="/movies">
+                                <span>
+                                    Movies
+                                </span>
+                            </Link>
+                            <Link className="text-white font-bold" to="/favorites">
+                                <span>
+                                    Favorite
+                                </span>
+                            </Link>
+                            <Link className="text-white font-bold" to="/bookmark">
+                                <span>
+                                    Bookmark
+                                </span>
+                            </Link>
+                    </div>
+                }
+                {
+                    isLogin === null && <></>
+                }
             </div>
         </nav>
     );
