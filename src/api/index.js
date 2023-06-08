a58d8197c89202410dcd8089dd28da631f138f85
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-06-08 23:14:33 +0700

    add constant for api

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "e791246398b5fdf3872c7b2063c9c65e";
+export const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNzkxMjQ2Mzk4YjVmZGYzODcyYzdiMjA2M2M5YzY1ZSIsInN1YiI6IjY0N2VlMzNjMGUyOWEyMmJkZmVkMDFiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zhg8q93xN1PpoJTO_AoGSxgtM2VUvOpeiyYwY5xblQY";
+export const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+export const BASE_IMAGE_URL_ORIGINAL = "https://image.tmdb.org/t/p/original";
