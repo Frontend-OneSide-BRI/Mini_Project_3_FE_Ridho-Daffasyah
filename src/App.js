b1c9f5845af59ad26c374e97ca12fb52e96a2c3a
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-06-08 23:11:56 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 import MoviesPages from './pages/Movies';
+import FavoritesPages from './pages/Favorites';
+import BookmarkPages from './pages/Bookmark';
+import DetailPages from './pages/Details';
 
-const App = () =>  {
+const App = () => {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/movies" element={<MoviesPages/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/movies" element={<MoviesPages />} />
+        <Route path="/favorites" element={<FavoritesPages />} />
+        <Route path="/bookmark" element={<BookmarkPages />} />
+        <Route path={`/detail/:id`} element={<DetailPages />} />
         {/* {
           data.map((item, index) => {
             return (
